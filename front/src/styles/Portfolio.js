b6218984929b914ac99ebfcf68e607c02f1b338a
Cho JeongTaek <cjt3591@gmail.com>
2023-04-26 15:56:57 +0900

    💄 Design:  프로젝트 폼 퍼블리싱-조정택-15:56

--- front/src/styles/Portfolio.js
+++ front/src/styles/Portfolio.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Wrap = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100%;
   background: linear-gradient(-135deg, rgb(200, 80, 192), rgb(65, 88, 208));
   padding-top: 100px;
 `;
--- front/src/styles/portfolio/Box.js
+++ front/src/styles/portfolio/Box.js
@@ -6,12 +6,12 @@ export const PortfolioBox = styled.div`
   border: 1px solid #ddd;
   border-radius: 8px;
   &:hover {
-    box-shadow: 0px 0px 5px 0.5px rgba(0, 0, 0, 0.2);
+    box-shadow: 0px 0px 15px 0.5px rgba(255, 255, 255, 0.2);
   }
 
   .css-ke5b6m-MuiButtonBase-root-MuiButton-root {
     display: block;
-    margin: 0 auto;
+    margin: 20px auto;
   }
 `;
 
@@ -19,4 +19,5 @@ export const Title = styled.h2`
   font-size: 18px;
   font-weight: 500;
   color: #fff;
+  margin-bottom: 30px;
 `;
