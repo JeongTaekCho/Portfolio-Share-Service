d193015b68e05ca99eb4a103107bb9454f74ab65
Cho JeongTaek <cjt3591@gmail.com>
2023-04-26 20:02:27 +0900

    💄 Design:  수상이력 퍼블리싱-조정택-20:02

--- front/src/components/portfolio/Box.js
+++ front/src/components/portfolio/Box.js
@@ -4,10 +4,12 @@ import Button from "@mui/material/Button";
 import { useState } from "react";
 import ProjectForm from "./forms/Project";
 import EducationForm from "./forms/Education";
+import AwardForm from "./forms/Award";
 
 export default function Box({ title }) {
   const [isProject, setIsProject] = useState(false);
   const [isEducation, setIsEducation] = useState(false);
+  const [isAward, setIsAward] = useState(false);
 
   const onClickBtn = () => {
     if (title === "프로젝트") {
@@ -16,6 +18,9 @@ export default function Box({ title }) {
     if (title === "학력") {
       setIsEducation((prev) => !prev);
     }
+    if (title === "수상이력") {
+      setIsAward((prev) => !prev);
+    }
   };
 
   return (
@@ -23,6 +28,7 @@ export default function Box({ title }) {
       <Title>{title}</Title>
       {isProject && <ProjectForm setIsProject={setIsProject} />}
       {isEducation && <EducationForm setIsEducation={setIsEducation} />}
+      {isAward && <AwardForm setIsAward={setIsAward} />}
       <Button variant="contained" color="success" onClick={onClickBtn}>
         {isProject || isEducation ? "-" : "+"}
       </Button>
