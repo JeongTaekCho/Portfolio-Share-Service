a023c20a10d8aaf0025aabac13697546e7dd9449
Cho JeongTaek <cjt3591@gmail.com>
2023-04-27 22:21:26 +0900

    ✨ Feat :  프로젝트 추가 기능 개발-조정택-0427_2221

--- front/src/components/portfolio/forms/Project.js
+++ front/src/components/portfolio/forms/Project.js
@@ -1,31 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import { BtnBox, Form } from "../../../styles/portfolio/forms/Project";
 import TextField from "@mui/material/TextField";
-import dayjs from "dayjs";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import Textarea from "@mui/joy/Textarea";
 import Button from "@mui/joy/Button";
+import axios from "axios";
+import { post } from "../../../api";
 
-export default function ProjectForm({ setIsProject }) {
+export default function ProjectForm({ setIsProject, isEdit }) {
+  const [projectName, setProjectName] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [content, setContent] = useState("");
+
+  const onChangeInput = (e) => {
+    const { name, value } = e.target;
+
+    if (name === "projectName") {
+      setProjectName(value);
+    } else if (name === "content") {
+      setContent(value);
+    }
+  };
+  const accessToken = sessionStorage.getItem("userToken");
+
+  const onChangeStartDate = ({ $d }) => {
+    setStartDate($d);
+  };
+  const onChangeEndDate = ({ $d }) => {
+    setEndDate($d);
+  };
+
   const onClickCancelForm = () => {
     setIsProject(false);
   };
 
+  const onClickAddProject = async () => {
+    try {
+      if (projectName && startDate && endDate && content) {
+        const data = {
+          projectName,
+          startDate,
+          endDate,
+          content,
+        };
+        const result = await post("projects", data);
+        setIsProject(false);
+        alert("프로젝트가 등록되었습니다.");
+      } else {
+        alert("빈칸을 채워주세요.");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <Form>
-      <TextField label="프로젝트명" id="outlined-start-adornment" sx={{ m: 1, width: "25ch" }} />
+      <TextField
+        label="프로젝트명"
+        name="projectName"
+        id="outlined-start-adornment"
+        sx={{ m: 1, width: "25ch" }}
+        onChange={onChangeInput}
+      />
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DemoContainer components={["DatePicker", "DatePicker"]}>
-          <DatePicker label="프로젝트 시작" />
-          <DatePicker label="프로젝트 마감" />
+          <DatePicker label="프로젝트 시작" onChange={onChangeStartDate} />
+          <DatePicker label="프로젝트 마감" onChange={onChangeEndDate} />
         </DemoContainer>
       </LocalizationProvider>
-      <Textarea minRows={2} placeholder="프로젝트 내용" />
+      <Textarea minRows={2} name="content" placeholder="프로젝트 내용" onChange={onChangeInput} />
       <BtnBox>
-        <Button>등록</Button>
+        <Button onClick={onClickAddProject}>등록</Button>
         <Button className="cancelBtn" onClick={onClickCancelForm}>
           취소
         </Button>
