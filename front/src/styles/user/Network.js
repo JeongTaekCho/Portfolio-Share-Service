638b6c56e62e2c27ebc0a26c1a9bb27293c50ec7
Cho JeongTaek <cjt3591@gmail.com>
2023-04-28 18:05:56 +0900

    💄 Design:  포트폴리오 네트워크 퍼블리싱-조정택-0428_1805

---
+++ front/src/styles/user/Network.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Wrap = styled.div`
+  width: 100%;
+  background: linear-gradient(-135deg, rgb(200, 80, 192), rgb(65, 88, 208));
+  padding-top: 100px;
+`;
+
+export const Container = styled.div`
+  width: 1200px;
+  margin: 0 auto;
+`;
+
+export const ContetnBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+`;
