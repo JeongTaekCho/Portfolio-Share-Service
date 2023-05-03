c92ed79917fb561895a4869e72cd48689578abcb
Cho JeongTaek <cjt3591@gmail.com>
2023-05-03 15:34:30 +0900

    🔨 Modify:  수상이력 기능 수정

--- back/src/services/awardService.js
+++ back/src/services/awardService.js
@@ -1,4 +1,4 @@
-import { Award } from "../db/models/Award"; 
+import { Award } from "../db/models/Award";
 
 class AwardService {
   static async addAward({ awardName, date, userId }) {
@@ -10,11 +10,10 @@ class AwardService {
   }
 
   static async getAward({ awardId }) {
-    console.log(awardId)
+    console.log(awardId);
     const award = await Award.findById({ awardId });
     if (!award) {
-      const errorMessage =
-        "해당 id를 가진 award 데이터는 없습니다.";
+      const errorMessage = "해당 id를 가진 award 데이터는 없습니다.";
       return { errorMessage };
     }
 
@@ -27,35 +26,34 @@ class AwardService {
   }
 
   static async changeAward({ awardId, awardName, date, userId }) {
-    console.log('awardService changeAward userId : ', userId);
+    console.log("awardService changeAward userId : ", userId);
     let award = await Award.findById({ awardId });
-    console.log('awardService changeAward award.userId : ', award.userId);
+    console.log("awardService changeAward award.userId : ", award.userId);
 
-    if(award.userId !== userId){
-      throw new Error('유효하지 않는 유저입니다.');
+    if (award.userId !== userId) {
+      throw new Error("유효하지 않는 유저입니다.");
     }
 
     if (!award) {
-      const errorMessage =
-        "해당 id를 가진 award 데이터는 없습니다.";
+      const errorMessage = "해당 id를 가진 award 데이터는 없습니다.";
       return { errorMessage };
     }
 
-    award = await Award.update({awardName, date})
+    award = await Award.update({ awardId, awardName, date });
 
     return award;
   }
 
   static async deleteAward(awardId, userId) {
-    console.log('awardService deleteAward awardId : ', awardId);
-    console.log('awardService deleteAward userId : ', userId);
+    console.log("awardService deleteAward awardId : ", awardId);
+    console.log("awardService deleteAward userId : ", userId);
     const award = await Award.findById({ awardId });
 
-    if(award.userId !== userId){
-      throw new Error('유효하지 않는 유저입니다.');
+    if (award.userId !== userId) {
+      throw new Error("유효하지 않는 유저입니다.");
     }
     await Award.deleteById(awardId);
-    
+
     return { status: "ok" };
   }
 }
