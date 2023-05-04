d49355fe4bd4452c56b962c700a4f5695e492dbd
Cho JeongTaek <cjt3591@gmail.com>
2023-05-04 21:58:53 +0900

    👷 Build :  서버 URL주소 수정

--- front/src/api.js
+++ front/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const backendPortNumber = "5001";
-const serverUrl = "http://localhost:" + backendPortNumber + "/";
+const serverUrl = "http://kdt-ai7-team06.elicecoding.com:" + backendPortNumber + "/";
 
 async function get(endpoint, params = "") {
   return axios.get(serverUrl + endpoint + "/" + params, {
